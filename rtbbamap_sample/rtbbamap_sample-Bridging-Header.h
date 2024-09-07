b5d23428ebf570130b618d0aaf51db77c033d821
drawrs <rizal.hilman67@gmail.com>
2024-09-07 19:11:49 +0700

    Setup build settings and briding header for C code

---
+++ rtbbamap_sample/rtbbamap_sample-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  rtbbamap_sample-Bridging-Header.h
+//  rtbbamap_sample
+//
+//  Created by Rizal Hilman on 07/09/24.
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//#include "NativeWrapper.hpp"
+//#import <sqlite3.h>
+

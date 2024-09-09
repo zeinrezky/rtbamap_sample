b567be5fbf71e822b545a8f6e50721b645dadab2
drawrs <rizal.hilman67@gmail.com>
2024-09-09 10:52:09 +0700

    Importing all necessary libraries

---
+++ rtbbamap_sample/DatabaseView.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseView.swift
+//  rtbbamap_sample
+//
+//  Created by Rizal Hilman on 09/09/24.
+//
+
+import Foundation
---
+++ rtbbamap_sample/VerticalScrollerView.swift
@@ -0,0 +1,8 @@
+//
+//  VerticalScrollerView.swift
+//  rtbbamap_sample
+//
+//  Created by Rizal Hilman on 09/09/24.
+//
+
+import Foundation
